b1acd2fa80282d8d6c64e260cc67ccdd99e171f4
ryan <ryanjgecelter@gmail.com>
2022-11-10 14:53:00 +0200

    add user context to app

---
+++ src/auth/UserContext.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+/** Context provides currentUser object and setter for it 
+ * throughout all components */
+
+const UserContext = React.createContext();
+
+export default UserContext;
