6dd3fb90c92c7fd2de0620d9cb2589fc022b0a9c
ryan <ryanjgecelter@gmail.com>
2022-11-13 12:06:03 +0200

    added routes and private routes components

---
+++ src/routes/PrivateRoutes.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from 'react-router-dom';
+import UserContext from "../auth-forms/UserContext";
+
+function PrivateRoutes({ children, exact, path, update }) {
+    const currentUser = useContext(UserContext);
+    if (!currentUser) {
+        return (
+            <Redirect to='/'></Redirect>
+        )
+    }
+    return (
+        <Route exact={exact} path={path} update={update}>
+            {children}
+        </Route>
+    )
+}
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { EventList, EventDetail } from '../events';
+import { LocationList, LocationDetail } from '../locations';
+import PrivateRoutes from './PrivateRoutes';
 
-function Routes({ login, signup }) {
+function Routes({ login, signup, update }) {
     return (
-        'TO BE CONTINUED...'
+        <Switch>
+            <PrivateRoutes path={'/events/:eventId'}><EventDetail /></PrivateRoutes>
+            <PrivateRoutes path={'/events'}><EventList /></PrivateRoutes>
+            <PrivateRoutes path={'/locations/:locationId'}><LocationDetail /></PrivateRoutes>
+            <PrivateRoutes path={'/locations'}><LocationList /></PrivateRoutes>
+            <PrivateRoutes path={'/profile-form'}><ProfileForm update={update} /></PrivateRoutes>
+            <Route path="/login-form"><LoginForm login={login} /></Route>
+            <Route path="/register"><RegisterForm signup={signup}></RegisterForm></Route>
+            <Redirect to='/'></Redirect>
+        </Switch>
     )
 }
 
