f567e0b9bee63cc5fb8c8eb338419a3d2a102e8f
ryan <ryanjgecelter@gmail.com>
2022-11-13 15:07:43 +0200

    added component imports to reacstract .. may remove this from index.js -- ask

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Accordion, Button, ButtonGroup, Carousel, Card, CloseButton, Form, List, ListGroup, Nav, Navbar, Spinner } from 'reactstrap';
+import { Accordion, Button, ButtonGroup, Carousel, Card, CloseButton, Form, FormGroup, Input, Label, List, ListGroup, Nav, Navbar, Spinner } from 'reactstrap';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
