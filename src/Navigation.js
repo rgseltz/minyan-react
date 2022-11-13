55755f0c6fff96bef4066988195c831d7eaa9dc0
ryan <ryanjgecelter@gmail.com>
2022-11-13 09:54:05 +0200

    cleaned navbar format/style

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,28 +6,30 @@ function Navigation({ logout }) {
     const currentUser = useContext(UserContext);
     function loggedInNav() {
         return (
-            <ul className="navbar-nav auto">
-                <li className="nav-item mr-4">
-                    <NavLink classname="nav-link" to="/events">
-                        Scheduled Minyanim
-                    </NavLink>
-                </li>
-                <li className="nav-item mr-4">
-                    <NavLink classname="nav-link" to="/events/new">
-                        Create New Minyan
-                    </NavLink>
-                </li>
-                <li className="nav-item mr-4">
-                    <NavLink classname="nav-link" to="/profile">
-                        Profile
-                    </NavLink>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/" onClick={logout}>
-                        Logout {currentUser.firstName || currentUser.lastName}
-                    </Link>
-                </li>
-            </ul>
+            <div className="navbar nav-right">
+                <ul className="navbar-nav auto">
+                    <li className="nav-item mr-4">
+                        <NavLink className="nav-link" to="/events">
+                            Scheduled Minyanim
+                        </NavLink>
+                    </li>
+                    <li className="nav-item mr-4">
+                        <NavLink className="nav-link" to="/events/new">
+                            Create New Minyan
+                        </NavLink>
+                    </li>
+                    <li className="nav-item mr-4">
+                        <NavLink className="nav-link" to="/profile">
+                            Profile
+                        </NavLink>
+                    </li>
+                    <li className="nav-item mr-auto">
+                        <NavLink className="nav-link" to="/" onClick={logout}>
+                            Logout {currentUser.firstName || currentUser.lastName}
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         )
     }
     function loggedOutNav() {
@@ -49,10 +51,12 @@ function Navigation({ logout }) {
 
     return (
         <nav className="Navigation navbar navbar-expand-md">
-            <Link className="navbar-brand" to="/">
-                Express Minyan
-            </Link>
-            {currentUser ? loggedInNav() : loggedOutNav}
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    Express Minyan
+                </Link>
+                {currentUser ? loggedInNav() : loggedOutNav}
+            </div>
         </nav>
     );
 }
