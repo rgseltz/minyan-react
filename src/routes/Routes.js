a7acc072619caeb8ba60e8f621514331cdf3c560
ryan <ryanjgecelter@gmail.com>
2022-11-14 05:59:26 +0200

    configured routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,18 +8,20 @@ import ProfileForm from '../auth-forms/ProfileForm';
 import LoginForm from "../auth-forms/LoginForm";
 import RegisterForm from "../auth-forms/RegisterForm";
 import PrivateRoutes from './PrivateRoutes';
+import Homepage from "../Homepage";
 
 function Routes({ login, signup, update }) {
     return (
         <Switch>
-            <PrivateRoutes path={'/events/:eventId'}><EventDetail /></PrivateRoutes>
-            <PrivateRoutes path={'/events'}><EventList /></PrivateRoutes>
+            <PrivateRoutes exact path={'/events/:eventId'}><EventDetail /></PrivateRoutes>
+            <PrivateRoutes exact path={'/events'}><EventList /></PrivateRoutes>
             <PrivateRoutes path={'/locations/:locationId'}><LocationDetail /></PrivateRoutes>
             <PrivateRoutes path={'/locations'}><LocationList /></PrivateRoutes>
             <PrivateRoutes path={'/profile-form'}><ProfileForm update={update} /></PrivateRoutes>
-            <Route path="/login"><LoginForm login={login} /></Route>
-            <Route path="/register"><RegisterForm signup={signup}></RegisterForm></Route>
-            <Redirect to='/'></Redirect>
+            <Route exact path={'/login'}><LoginForm login={login} /></Route>
+            <Route exact path={'/signup'}><RegisterForm signup={signup}></RegisterForm></Route>
+            <Route path={'/'}><Homepage /></Route>
+            {/* <Redirect to='/' /> */}
         </Switch>
     )
 }
