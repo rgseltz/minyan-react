c1fb07eaa6eb62bce4ede764dfe39ebcdc93f788
ryan <ryanjgecelter@gmail.com>
2022-11-14 06:01:57 +0200

    typo useContext in PrivateRoute

---
+++ src/Homepage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Homepage() {
+    return (
+        <h1>Welcome to Minyan Maker Express!</h1>
+    )
+}
+
+export default Homepage;
--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import UserContext from "../auth-forms/UserContext";
 
 function PrivateRoutes({ children, exact, path, update }) {
-    const currentUser = useContext(UserContext);
+    const { currentUser } = useContext(UserContext);
     if (!currentUser) {
         return (
             <Redirect to='/'></Redirect>
